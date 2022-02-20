5b6d4cdabc4e7fed8e7418388db5e6e9f3a57d9f
Ricardo <ric.dalvarez@hotmail.com>
2022-02-20 14:52:50 +0100

    add fetch data from backand with useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,8 @@ function App() {
 
       <div className="App">
         <h1 className= "italic font-mono text-8xl text-slate-50 tracking-normal text-center align-middle p-5 bg-gradient-to-r from-red-600 to-gray-700">Fuscas</h1>
-        <AddCar/>       
+      <AddCar />   
+      <Cars/>
       </div>
     
   );
--- client/src/Components/cars/Cars.js
+++ client/src/Components/cars/Cars.js
@@ -1,12 +1,33 @@
 import Car from "../car/Car"
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Cars() {
-	let fuscas = ["a", "b", "c", "d", "e"]
+
+	let [carStore, setCarStore] = useState([{}])
+	
+	const apiURL = "http://localhost:3001/cars";
+
+			const fetchData = async () => {
+				const response = await axios.get(apiURL);
+				setCarStore(response);
+				console.log(response);
+			}
+
+	useEffect(() => {
+		fetchData();
+ });
 
 	return (
-		<>
-			<h1>MY CARS</h1>
-			<Car></Car>
-		</>
+		<div>	{carStore && carStore.map((car) => {
+			return (
+				<div>
+					<h1>{ car.tipo}</h1>
+				</div>
+			)
+		})}
+			{/* <Car></Car> */}
+		</div>
 	)
 }
